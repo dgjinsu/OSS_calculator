ee271c5d563bde787e85b6965b937c031788a02a
KIMJINSU <97269799+dgjinsu@users.noreply.github.com>
2022-05-04 09:22:35 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup (
+	include_package_data= True,
+	name = 'jscalculator',
+	version='1.0.0',
+	description='oss_jinsu_calculator',
+	author='dgjinsu',
+	author_email='1923915@donga.ac.kr',
+	url="https://github.com/dgjinsu/OSS_calculator.git",
+	install_requires = ['pytest'],
+	long_description='oss-dev calculator python module',
+	long_description_content_type = 'text/markdown',
+	classifiers=
+	[
+		"Programming Language :: Python :: 3",
+		"Operating System :: OS Independent",
+	],
+)
